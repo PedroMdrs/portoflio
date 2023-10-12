77065c91406974fe95994a702946b243f233d43d
Pedro Medeiros <contareservadopedro@Outlook.com>
2023-10-12 14:51:16 -0300

    menu mobile fixed

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -11,28 +11,74 @@ const Header = () => {
   const [mobileButton, setMobileButton] = React.useState(false);
   const { theme, setTheme } = useThemeContext();
   return (
-    <header>
-      <nav
-        className={`${styles.nav} 
-         ${styles[theme]}`}
-      >
+    <header className={styles[theme]}>
+      <div className={styles.content}>
         <NavLink className={`${styles.logo}  ${styles[theme]}`} to={"/"} end>
           Pedro Medeiros
         </NavLink>
-
-        <ul className={styles.routes}>
-          <li>
-            <NavLink className={styles.route} to={"projects"} end>
-              Projects
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className={styles.route} to={"skills"} end>
-              Skills
-            </NavLink>
-          </li>
-        </ul>
-
+        <nav
+          className={` 
+         ${styles[theme]} ${mobileButton ? styles.open : ""}`}
+        >
+          <ul className={styles.routes}>
+            <li>
+              <NavLink className={styles.route} to={"projects"} end>
+                Projects
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className={styles.route} to={"skills"} end>
+                Skills
+              </NavLink>
+            </li>
+          </ul>
+          <ul className={styles.iconsMobile}>
+            <li>
+              <a className={styles[theme]} href="https://github.com/PedroMdrs">
+                <Github />
+              </a>
+            </li>
+            <li>
+              <a
+                className={styles[theme]}
+                href="https://www.linkedin.com/in/pedro-henrique-medeiros/"
+              >
+                <Linkedin />
+              </a>
+            </li>
+            <li className={styles[theme]}>
+              {theme === "dark" ? (
+                <div
+                  onClick={() => {
+                    setTheme(() => {
+                      localStorage.setItem("theme", JSON.stringify("light"));
+                      return "light";
+                    });
+                    document
+                      .querySelector("body")
+                      ?.setAttribute("data-theme", "light");
+                  }}
+                >
+                  <Moon />
+                </div>
+              ) : (
+                <div
+                  onClick={() => {
+                    setTheme(() => {
+                      localStorage.setItem("theme", JSON.stringify("dark"));
+                      return "dark";
+                    });
+                    document
+                      .querySelector("body")
+                      ?.setAttribute("data-theme", "dark");
+                  }}
+                >
+                  <Sun />
+                </div>
+              )}
+            </li>
+          </ul>
+        </nav>{" "}
         <div className={styles.icons}>
           <a href="https://github.com/PedroMdrs">
             <Github />
@@ -70,88 +116,15 @@ const Header = () => {
             </div>
           )}
         </div>
-      </nav>
-
-      <nav
-        className={`${styles.mobileNav} ${mobileButton ? styles.open : ""} ${
-          styles[theme]
-        }`}
-      >
-        <div className={styles[theme]}>
-          <NavLink className={`${styles.logo}  ${styles[theme]}`} to={"/"} end>
-            Pedro Medeiros
-          </NavLink>
-          <button
-            className={`${styles.burger} ${styles[theme]} ${
-              mobileButton ? styles.open : ""
-            }`}
-            onClick={() => setMobileButton((value) => !value)}
-          >
-            <div className={`${styles.burgerLines} ${styles[theme]}`}></div>
-          </button>
-          <div
-            className={`${mobileButton ? "" : styles.menuClosed} ${
-              styles.mobileMenu
-            }`}
-          >
-            <ul className={styles.routes}>
-              <li>
-                <NavLink className={styles.route} to={"projects"} end>
-                  Projects
-                </NavLink>
-              </li>
-              <li>
-                <NavLink className={styles.route} to={"skills"} end>
-                  Skills
-                </NavLink>
-              </li>
-            </ul>
-
-            <div className={styles.icons}>
-              <a className={styles[theme]} href="https://github.com/PedroMdrs">
-                <Github />
-              </a>
-              <a
-                className={styles[theme]}
-                href="https://www.linkedin.com/in/pedro-henrique-medeiros/"
-              >
-                <Linkedin />
-              </a>
-              {theme === "dark" ? (
-                <div
-                  className={styles[theme]}
-                  onClick={() => {
-                    setTheme(() => {
-                      localStorage.setItem("theme", JSON.stringify("light"));
-                      return "light";
-                    });
-                    document
-                      .querySelector("body")
-                      ?.setAttribute("data-theme", "light");
-                  }}
-                >
-                  <Moon />
-                </div>
-              ) : (
-                <div
-                  className={styles[theme]}
-                  onClick={() => {
-                    setTheme(() => {
-                      localStorage.setItem("theme", JSON.stringify("dark"));
-                      return "dark";
-                    });
-                    document
-                      .querySelector("body")
-                      ?.setAttribute("data-theme", "dark");
-                  }}
-                >
-                  <Sun />
-                </div>
-              )}
-            </div>
-          </div>
-        </div>
-      </nav>
+        <button
+          className={`${styles.burger} ${styles[theme]} ${
+            mobileButton ? styles.open : ""
+          }`}
+          onClick={() => setMobileButton((value) => !value)}
+        >
+          <div className={`${styles.burgerLines} ${styles[theme]}`}></div>
+        </button>
+      </div>
     </header>
   );
 };
